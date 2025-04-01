68d538b7c591ddc34813fc7ba51faf0584156726
O'Dwyer, James (AAHL, Geelong ACDP) <James.O'Dwyer@csiro.au>
2025-04-01 17:11:13 +1100

    Update User documentation main sections 4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,23 +1,28 @@
+# ==============================================================================
+# MetaDIVE Sphinx Configuration File
+# ==============================================================================
+
+# ??? BUILD COMMANDS:
+# ------------------------------------------------------------------------------
+# ?? Build locally (for testing or file:// viewing):
+# sphinx-build -b html
+#
+# ?? Build for GitHub Pages (relative path safe under /MetaDIVE/):
+# GITHUB_PAGES=1 sphinx-build -b html docs/source docs/html
+# cp -r docs/html/* docs/
+# ==============================================================================
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'MetaDIVE'
 copyright = "2025, James O'Dwyer"
 author = "James O'Dwyer"
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
@@ -27,11 +32,16 @@ extensions = [
 
 templates_path = ['_templates']
 exclude_patterns = []
-
 language = 'English'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# -- Environment-specific options --------------------------------------------
+on_github_pages = os.environ.get("GITHUB_PAGES", "") == "1"
 
+if on_github_pages:
+    html_baseurl = "https://james-odwyer.github.io/MetaDIVE/"
+else:
+    html_baseurl = "/"
+
+# -- HTML output -------------------------------------------------------------
 html_theme = 'alabaster'
 html_static_path = ['_static']
