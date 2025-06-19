ce48608467f19bf61d2e85ce61fc0d632c754d45
James-ODwyer <74756289+James-ODwyer@users.noreply.github.com>
2025-06-19 12:59:08 +1000

    Update download_kraken_DBs.sh
    Lowering memory requirements for kraken download

--- databases/download_kraken_DBs.sh
+++ databases/download_kraken_DBs.sh
@@ -4,8 +4,8 @@
 #SBATCH --nodes 1                        # nodes to use on the hpc, 1 node= max 64CPUs so leave as 1. 
 #SBATCH --ntasks-per-node 1              # ntasks per node (not needed to play around with for the pipeline. the pipeline will allocate all resources as best needed)
 #SBATCH --cpus-per-task 1               # total number of CPUs to allocate. depending on size of data and urgency, 12-48  
-#SBATCH --mem 12G                       # Total memory. Can require a lot particularly if you want to run trinity! between 80 and 180 depending on complexity of data
-#SBATCH --time 4:00:00                 # Time requirements hh/mm/ss would recommend around 100 hours for large datasets. if it doesn't complete you can always launch the script again
+#SBATCH --mem 4G                       # Total memory. Can require a lot particularly if you want to run trinity! between 80 and 180 depending on complexity of data
+#SBATCH --time 24:00:00                 # Time requirements hh/mm/ss would recommend around 100 hours for large datasets. if it doesn't complete you can always launch the script again
 #SBATCH --partition io  
 
 
